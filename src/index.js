ed70f653aee86eec980fb69fae4df9efaf4aaee8
SamuelLo <samuello300@gmail.com>
2023-08-14 14:38:55 -0700

    learning react to build a project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// react app component gets rendered into root through this file
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
